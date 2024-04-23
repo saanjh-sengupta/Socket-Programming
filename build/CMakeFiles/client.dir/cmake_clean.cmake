2b8c6668848efd4b62cafd2dfe12abbfbc1b3642
saanjh-sengupta <saanjhsengupta@outlook.com>
2024-04-23 12:11:19 +0530

    Socket Version 6: CMake Build

---
+++ build/CMakeFiles/client.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/client.dir/MainClient.cpp.o"
+  "CMakeFiles/client.dir/source/client.cpp.o"
+  "CMakeFiles/client.dir/source/server.cpp.o"
+  "client"
+  "client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/server.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/server.dir/MainServer.cpp.o"
+  "CMakeFiles/server.dir/source/client.cpp.o"
+  "CMakeFiles/server.dir/source/server.cpp.o"
+  "server"
+  "server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
